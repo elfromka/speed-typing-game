28ee0663706b52858abe328ea2409dbc9e9eae92
Sebestyen Orsolya <elfromka@gmail.com>
2022-08-02 16:00:56 +0300

    Uploading some extra icons, pngs, modified manifest and index.html head, added functionality to the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,71 @@
+import { useState, useEffect, ChangeEvent, useRef } from "react";
 import "./assets/scss/styles.scss";
 
 const App: React.FC = (): JSX.Element => {
+    const TOTAL_SECONDS: number = 10;
+
+    const [inputData, setInputData] = useState<string>("");
+    const [remainingSeconds, setRemainingSeconds] =
+        useState<number>(TOTAL_SECONDS);
+    const [start, setStart] = useState<boolean>(false);
+    const [totalWords, setTotalWords] = useState<number>(0);
+
+    useEffect(() => {
+        if (remainingSeconds > 0 && start) {
+            setTimeout(() => {
+                setRemainingSeconds(
+                    (prevremainingSeconds) => prevremainingSeconds - 1
+                );
+            }, 1000);
+        } else {
+            endGame();
+        }
+    }, [remainingSeconds, start]);
+
+    const countWords = (text: string): number =>
+        text !== "" ? text.trim().split(" ").length : 0;
+
+    const startGame = (): void => {
+        setRemainingSeconds(TOTAL_SECONDS);
+        setStart((prevStart) => !prevStart); // or simply pass true
+        setTotalWords(0);
+        setInputData("");
+    };
+
+    const endGame = (): void => {
+        setStart(false);
+        setTotalWords(countWords(inputData));
+    };
+
+    const handleOnChange = (event: ChangeEvent<HTMLTextAreaElement>): void => {
+        const { value }: { value: string } = event.target;
+
+        setInputData(() => value);
+    };
+
     return (
         <main className="container">
             <section className="game">
                 <h1 className="game__title">Speed Typing Game</h1>
-                <textarea className="game__textarea" value="" />
-                <h2 className="game__timer">Remaining time: 0</h2>
-                <button className="game__start-button">Start</button>
+                <textarea
+                    disabled={!start}
+                    value={inputData}
+                    name="inputData"
+                    onChange={handleOnChange}
+                    className="game__textarea"
+                />
+                <h2 className="game__timer">
+                    Remaining time: {remainingSeconds}
+                </h2>
+                <button
+                    disabled={start}
+                    onClick={startGame}
+                    className="game__start-button"
+                >
+                    Start
+                </button>
                 <h2 className="game__total-typeed-words">
-                    Total words typed: 5
+                    Total words typed: {totalWords}
                 </h2>
             </section>
         </main>
